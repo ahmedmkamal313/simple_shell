5daf3ea5db38e827a5ec2eaac4d874944e6992fa
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-05-13 11:28:46 -0700

    added a function to compare two strings

--- handle_string_0.c
+++ handle_string_0.c
@@ -21,3 +21,30 @@ char *_strcat(char *dest, char *src)
 
 	return (p); /*return the original pointer to dest. */
 }
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: the 1st string to be compared.
+ * @s2: the second string.
+ * Return: positing integer if s1 > s2
+ * 0 if s1 = s2
+ * negative integer if s1 < s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2) /* loop through the characters of the both strings */
+	{
+		/* if characters are different, return the difference */
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		/* otherwise move to the next characters */
+		s1++;
+		s2++;
+	}
+
+	/* if one string is longer than other, return the difference */
+	return (*s1 - *s2);
+}
