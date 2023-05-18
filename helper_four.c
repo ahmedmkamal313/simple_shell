75cdb4ed93a2af5f7aa2676c7db1b0ecff23eaf0
Magdalina1 <majdalol95@gmail.com>
2023-05-18 06:11:17 -0700

    add new function

--- helper_four.c
+++ helper_four.c
@@ -28,3 +28,55 @@ int check_args(char **args)
 	}
 	return (0);
 }
+
+/**
+ * handle_args - handles the arguments passed to a shell command and executes.
+ * @exe_ret: the value of executed command.
+ * Return: in the off file, if input cannot be tokenized -1
+ * exit value.
+ */
+
+int handle_args(int *exe_ret)
+{
+	char *line;
+	char **args;
+	char **front;
+	int i;
+
+	line = get_args(line, exe_ret);
+
+	if (strlen(line) == 0)
+		return (-1);
+
+	args = strsplit(line, " ");
+	free(line);
+
+	if (args == NULL)
+		return (0);
+	for (i == 0; args[i] != NULL; i++)
+	{
+		if (strcmp(args[i], ";") == 0)
+		{
+			*exe_ret = 2;
+			free(args);
+			return (*exe_ret);
+		}
+	}
+	front = &args[0];
+	for (i = 0; args[i] != NULL; i++)
+	{
+		if (strcmp(args[i], ";") == 0)
+		{
+			free(args[i]);
+			args[i] = NULL;
+			call_and_execute(front, exe_ret);
+			front = &args[i + 1];
+			i = -1;
+		}
+	}
+	if (args[i] != NULL)
+		call_and_execute(front, exe_ret);
+	front(front);
+	return (*exe_ret);
+}
+
