82e3e0ece649a60a312610238d192ebdfd9dcac0
Magdalina1 <majdalol95@gmail.com>
2023-05-18 10:11:06 -0700

    add new function

--- splitting.c
+++ splitting.c
@@ -19,3 +19,42 @@ int token_len(char *str, char *delim)
 	}
 	return (len);
 }
+
+/**
+ * count_tokens - This function takes a string and
+ * a delimiter character as arguments.
+ * @str: the string.
+ * @delim: the delimiter character.
+ * Return: the number of tokens.
+ */
+
+int count_tokens(char *str, char *delim)
+{
+	int index = 0;
+	int num_tokens = 0;
+	int len = 0;
+
+	while (str[index] != '\0')
+	{
+		len++;
+		index++;
+	}
+
+	index = 0;
+
+	(index < len)
+	{
+		if (str[index] != *delim)
+		{
+			num_tokens++;
+			index += token_len(str + index, delim);
+		}
+		else
+		{
+			index++;
+		}
+	}
+
+	return (num_tokens);
+}
+
