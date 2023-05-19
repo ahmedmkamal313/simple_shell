99d75e7f731de8024312ac451c0aa3e6b9a696db
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-05-19 06:24:40 -0700

    new functions to add to lists

--- lists.c
+++ lists.c
@@ -1,6 +1,73 @@
 #include "main.h"
 
 /**
+ * add_node_end - adds a new node at the end of a linked list of directories
+ * @head: the head of the list
+ * @dir: path for the new node
+ * Return: new node , null if error
+ */
+
+list_t *add_node_end(list_t **head, char *dir)
+{
+	list_t *last; /*pointer to store the last node */
+	/* allocate memory fr the new node */
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (!new_node) /* check f meory allocation failed */
+		return (NULL);
+	new_node->dir = dir; /* assign dir name to new node */
+	new_node->next = NULL; /* set next pointer of new node to null*/
+	if (*head) /** check if the list is not empty */
+	{
+		last = *head;
+		while (last->next != NULL) /* loop until the last node*/
+			last = last->next; /* move to next node */
+		last->next = new_noce; /* link last node to new node */
+	}
+	else
+		/* if the list is empty, set the head to new node */
+		*head = new_node;
+	return (new_node); /* return the new node */
+}
+
+/**
+ * add_alias_end - adds a new alias at the end of a linked list of aliases
+ * @head: pointer to the head of the list
+ * @name: ame of the new alias to add
+ * @value: value of the new alias
+ * Return: the new node, NULL if fail
+ */
+
+alias_t *add_alias_end(alias_t **head, char *name, char *value)
+{
+	/* Allocate memory for the new alias */
+	alias_t *new_node = malloc(sizeof(alias_t));
+	alias_t *last; /* pointer to store the last alisas */
+
+	if (!new_node) /* Check if memory allocation failed */
+		return (NULL);
+	new_node->next = NULL; /* set the next to NULL */
+	/* Allocate memory for the alias name */
+	new_node->name = malloc(size(char) * (_strlen(name) + 1));
+	if (!new_node->name) /* Check if memory allocation failed */
+	{
+		free(new_node); /* free te nea alias */
+		return (NULL);
+	}
+	new_node->value = value; /* Assign the alias value to the new alias */
+	_strcpy(new_node->name, name); /*Copy the alias name to the new alias */
+	if (*head) /* check if the list is not empty */
+	{
+		last = *head; /* Initialize the last pointer to the head of the list*/
+		while (last->next != NULL) /*Loop until the last alias is found */
+			last = last->next; /*Move the last pointer to the next alias */
+		last->next = new_node; /* Link the last alias to the new alias */
+	}
+	else
+		*head = new_node;
+	return (new_node);
+}
+/**
  * free_list - frees a lined list of directories
  * @head: the head of list
  */
