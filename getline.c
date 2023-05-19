ccc4ea26612347d395e3540b676d67ca65977ece
Magdalina1 <majdalol95@gmail.com>
2023-05-19 10:40:49 -0700

    add new function

--- getline.c
+++ getline.c
@@ -50,3 +50,55 @@ void *_realloc(void *ptr, size_t old_size, size_t new_size)
 	/* Return the pointer to the new memory block */
 	return (new_ptr);
 }
+
+/**
+ * _getline - reads input.
+ * @lineptr: buffer to store the input.
+ * @n: the size.
+ * @stream: the stream to read from.
+ * Return: the number of bytes.
+ */
+
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
+	char *buffer; /* Temporary buffer to store input */
+	size_t b = 0; /* Size of buffer */
+	int c; /* Character read from stream */
+
+	/* If lineptr or n or stream is NULL, return -1 */
+	if (lineptr == NULL || n == NULL || stream == NULL)
+		return (-1);
+
+	/* Allocate memory for buffer */
+	buffer = malloc(120 * sizeof(char));
+	if (buffer == NULL)
+		return (-1);
+
+	/* Flush the output buffer of stream */
+	fflush(stream);
+
+	/* Read characters from stream until newline or EOF or error */
+	while ((c = read(fileno(stream), &c, 1)) > 0 && c != '\n')
+	{
+		/* Store character in buffer */
+		buffer[b] = c;
+		b++;
+		/* Reallocate memory for buffer if needed */
+		if (b >= 120)
+			buffer = realloc(buffer, (b + 1) * sizeof(char));
+			if (buffer == NULL)
+
+				return (-1);
+	}
+	/* Add null terminator to buffer */
+	buffer[b] = '\0';
+	/* Reassign lineptr and n using assign_lineptr function */
+	assign_lineptr(lineptr, n, buffer, b + 1);
+
+	/* If error occurred, return -1 */
+	if (c == -1)
+		return (-1);
+
+	/* Return number of bytes read */
+	return (b + 1);
+}
