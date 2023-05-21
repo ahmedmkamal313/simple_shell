2b79876d29adbead7e3dee51f8428bb3c184725f
Magdalina1 <majdalol95@gmail.com>
2023-05-21 05:41:23 -0700

    builtin_two.c

---
+++ builtin_two.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * help_help - Displays information on the shellby builtin command 'help'.
+ */
+void help_help(void)
+{
+	char *msg = "help: help\n\tSee all possible Shellby builtin commands.\n";
+
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = "STATUS argument is the integer used to exit the shell.";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " If no argument is given, the command is interpreted as";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+	msg = " exit 0.\n";
+	write(STDOUT_FILENO, msg, _strlen(msg));
+}
