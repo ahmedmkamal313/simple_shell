2032982d54bbc6357eadabc98118d4575ff6ab0b
ahmedmkamal313 <ahmedmkamal313@gmail.com>
2023-05-21 12:49:54 -0700

    new function sets or updates an environment variable in the shell environment

--- builtin_three.c
+++ builtin_three.c
@@ -29,3 +29,54 @@ int shellby_env(char **args, char __attribute__((__unused__)) **front)
 	return (0); /* return 0 on success */
 }
 
+/**
+ * shellby_setenv - sets or updates an environment variable
+ * in the shell environment
+ * @args: array of arguments.
+ * @front: pointer to the beginning of args.
+ * Return: -1 if error , 0.
+ */
+
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front)
+{
+	/* pointers to the env variable new env array and the new value */
+	char **env_var == NULL, **new_environ, **new_value;
+	size_t size;/* the size of the current enviroment */
+	int index; /* loop variable */
+
+	/* if no argument or only one argument is given return error */
+	if (!args[0] || !args[1])
+		return (create_error(args, -1));
+	/* allocate memory for the new value */
+	new_value = malloc(_strlen(args[0]) + 1 + _strlen(args[1]) + 1);
+	if (!new_value)/* if allocation fails, return an error */
+		return (create_error(args, -1));
+	_strcpy(new_value, args[0]); /* copy the name to new value */
+	_strcat(new_value, "="); /*append an equal sign to the new value*/
+	_strcat(new_value, args[1]);/* append the value the new value */
+	/* get the pointer to the env variable to bet set or update */
+	env_var = _getenv(args[0]);
+	if (env_var) /* if the variable already exists */
+	{
+		free(*env_var); /* free it's memory */
+		*env_var = new_value; /* assign the new value to it */
+		return (0); /* return 0 on success */
+	}
+	/* count the number of environment variables */
+	for (size = 0; environ[size]; size++)
+		;
+	/* allocate memory for the new environment array with one extra slot */
+	new_environ = malloc(sizeof(char *) * (size + 2));
+	if (!new_environ) /* if allocation fails */
+	{
+		free(new_value); /* free the new value memory */
+		return (create_error(args, -1)); /* return an error */
+	}
+	for (index = 0; environ[index]; index++)
+		new_environ[index] = environ[index];
+	free(environ); /*free the old environment array*/
+	environ = new_environ;
+	environ[index] = new_value;
+	environ[index + 1] = NULL;
+	return (0); /* return 0 on success */
+}
