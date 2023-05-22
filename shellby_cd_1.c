b03c5afe440502d98576228211221f131c77b8e1
Magdalina1 <majdalol95@gmail.com>
2023-05-22 05:49:10 -0700

    add new function

--- shellby_cd_1.c
+++ shellby_cd_1.c
@@ -10,3 +10,32 @@ void chdir_to_home(void)
 	if (_getenv("HOME") != NULL)
 		chdir(*(_getenv("HOME")) + 5);
 }
+
+/**
+ * update_env - updates the environment variables OLDPWD and PWD.
+ * @oldpwd: the previous working directory.
+ * @pwd: the current working directory.
+ * Return: 0 on success, -1 on failure.
+ */
+
+int update_env(char *oldpwd, char *pwd)
+{
+	char **dir_info;
+
+	dir_info = malloc(sizeof(char *) * 2);
+	if (!dir_info)
+		return (-1);
+
+	dir_info[0] = "OLDPWD";
+	dir_info[1] = oldpwd;
+	if (shellby_setenv(dir_info, dir_info) == -1)
+		return (-1);
+
+	dir_info[0] = "PWD";
+	dir_info[1] = pwd;
+	if (shellby_setenv(dir_info, dir_info) == -1)
+		return (-1);
+
+	free(dir_info);
+	return (0);
+}
