9d40938a3dc4512e679606aefdcb3131c4d08612
Magdalina1 <majdalol95@gmail.com>
2023-05-24 05:00:50 -0700

    fixing errors of output

--- file_commands.c
+++ file_commands.c
@@ -77,26 +77,58 @@ int read_file(char *file_path, char **line, int *exe_ret)
 
 int proc_file_commands(char *file_path, int *exe_ret)
 {
+	ssize_t file, b_read, i;
+	unsigned int line_size = 0;
+	unsigned int old_size = 120;
 	char *line, **args, **front;
-	int line_size, ret;
-	int i;
+	char buffer[120];
+	int ret;
 
 	history = 0;
-	line_size = read_file(file_path, &line, exe_ret);
-	if (line_size == -1)
+	file = open(file_path, O_RDONLY);
+	if (file == -1)
+	{
+		*exe_ret = cant_open(file_path);
+		return (*exe_ret);
+	}
+	line = malloc(sizeof(char) * old_size);
+	if (!line)
 		return (-1);
-
+	do {
+		b_read = read(file, buffer, 119);
+		if (b_read == 0 && line_size == 0)
+			return (*exe_ret);
+		buffer[b_read] = '\0';
+		line_size += b_read;
+		line = _realloc(line, old_size, line_size);
+		_strcat(line, buffer);
+		old_size = line_size;
+	} while (b_read);
 	for (i = 0; line[i] == '\n'; i++)
 		line[i] = ' ';
-	for (i = 0; i < line_size; i++)
-		line[i] = (line[i] == '\n') ? ';' : line[i];
+	for (; i < line_size; i++)
+	{
+		if (line[i] == '\n')
+		{
+			line[i] = ';';
+			for (i += 1; i < line_size && line[i] == '\n'; i++)
+				line[i] = ' ';
+		}
+	}
 	variable_replacement(&line, exe_ret);
 	handle_line(&line, line_size);
 	args = _strtok(line, " ");
 	free(line);
-	if (!args || check_args(args) != 0)
-		return (*exe_ret = 2);
+	if (!args)
+		return (0);
+	if (check_args(args) != 0)
+	{
+		*exe_ret = 2;
+		free_args(args, args);
+		return (*exe_ret);
+	}
 	front = args;
+
 	for (i = 0; args[i]; i++)
 	{
 		if (_strncmp(args[i], ";", 1) == 0)
@@ -108,7 +140,9 @@ int proc_file_commands(char *file_path, int *exe_ret)
 			i = 0;
 		}
 	}
+
 	ret = call_args(args, front, exe_ret);
+
 	free(front);
 	return (ret);
 }
